840a997d0a8291bcedb7a1aa2fdbe312b37ab567
KingWndr15 <abrahameyaife@gmail.com>
2024-06-29 16:48:09 +0100

    done

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Weather App</h1>
+    <p>Enter City</p>
+    <input type="text" v-model="city">
+    <button @click="getWeather">Get Weather</button>
+
+    <p>{{ weatherStore.weather }}</p>
+    <p>{{ weatherStore.degree }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useWeatherStore } from "@/store/store";
+
+const weatherStore = useWeatherStore();
+const city = ref(""); 
+
+const getWeather = async () => {
+  await weatherStore.fetchWeather(city.value); 
+};
+
+const { fetchWeather } = weatherStore;
+</script>
---
+++ store/store.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.weatherapi.com/v1',
+  params: {
+    key: 'ae4c48ab7cd141b282d151037242906',
+  },
+});
+
+export const useWeatherStore = defineStore("cities", {
+  state: () => ({ city: "", weather: "", degree: "", error: null }),
+  actions: {
+    async fetchWeather(city) {
+        try {
+            this.city = city
+            const response = await axiosInstance.get('/current.json', {
+                params: {
+                  q: city,
+                },
+              });
+              this.weather = response.data.current.condition.text;
+              this.degree = `Feels like ${response.data.current.feelslike_c}°`;
+        } catch (error) {
+            this.error = error.message;
+        }
+    }
+  }
+});
